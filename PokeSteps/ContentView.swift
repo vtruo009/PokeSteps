0b7bffb911a420f5e6a13ef79e7540157c4c116d
Van Truong <van.t414@gmail.com>
2024-02-09 14:16:10 -0800

    Initial Commit

---
+++ PokeSteps/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PokeSteps
+//
+//  Created by Van Truong on 2/9/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PokeSteps/PokeStepsApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokeStepsApp.swift
+//  PokeSteps
+//
+//  Created by Van Truong on 2/9/24.
+//
+
+import SwiftUI
+
+@main
+struct PokeStepsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
