ee6b68a8edcbd025496af52460e6428d22c62874
Van Truong <van.t414@gmail.com>
2024-02-10 00:02:55 -0800

    complete backbone of StarterSelectionView

--- PokeSteps/Views/StarterSelectionView.swift
+++ PokeSteps/Views/StarterSelectionView.swift
@@ -9,13 +9,38 @@ import SwiftUI
 
 struct StarterSelectionView: View {
     @StateObject var pokemonData: PokemonData = PokemonData()
+    let buttonW: Double = 100
+    let buttonH: Double = 50
+    let buttonColor: Color = Color(red: 42/255, green: 117/255, blue: 187/255)
+    var selectedStarter: Pokemon? = nil
     
     var body: some View {
         NavigationStack {
-            // Placeholder
-            ForEach(Pokemon.starters) { starter in
-                PokemonView(pokemon: starter)
+            VStack {
+                Spacer()
+                ForEach(Pokemon.starters) { starter in
+                    Button {
+                        print("Selected \(starter.name)!")
+                    } label: {
+                        PokemonView(pokemon: starter)
+                    }
+                }
+                Spacer()
+                Button {
+                    print("Started Selected. Go to next view!")
+                } label: {
+                    RoundedRectangle(cornerRadius: 15, style: /*@START_MENU_TOKEN@*/.continuous/*@END_MENU_TOKEN@*/)
+                        .frame(width: buttonW, height: buttonH)
+                        .foregroundColor(buttonColor)
+                        .overlay(
+                            Text("Confirm")
+                                .foregroundColor(.white)
+                                .font(.system(size: 14, weight: .bold, design: .monospaced))
+                        )
+                }
+                Spacer()
             }
+            .navigationTitle(Text("Select Starter"))
         }
     }
 }
