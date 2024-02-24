e953919f1f718f38b7df45ddfec980c07f9cd0b9
Van Truong <van.t414@gmail.com>
2024-02-24 00:00:24 -0800

    revert changes

---
+++ PokeSteps/Views/Pokemon/HiddenPokemonView.swift
@@ -0,0 +1,18 @@
+//
+//  HiddenPokemonView.swift
+//  PokeSteps
+//
+//  Created by Van Truong on 2/23/24.
+//
+
+import SwiftUI
+
+struct HiddenPokemonView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HiddenPokemonView()
+}
--- PokeSteps/Views/Pokemon/PokemonView.swift
+++ PokeSteps/Views/Pokemon/PokemonView.swift
@@ -9,21 +9,28 @@ import SwiftUI
 
 struct PokemonView: View {
     @Environment(\.dismiss) var dismiss
+    @EnvironmentObject var pokemonManager: PokemonManager
     @State var viewStyle: ViewStyle
+    @State var selected: Bool = false
     @Binding var pokemon: Pokemon
     private let dimension: Double = 140
-    private let defaultUrl = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/0.png"
     
     var body: some View {
+        let pokemonID = pokemon.isUnlocked ? pokemonManager.getPokemonID(pokemon: pokemon) : 0
+        let url = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/\(pokemonID).png"
+        
         VStack {
             Button {
                 pokemon.isUnlocked = true
+                selected = true
                 if viewStyle == .surprise {
-                    dismiss()
+                    DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+                                  dismiss()
+                            }
                 }
                 debugPrint("\(pokemon.name) unlocked!")
             } label: {
-                AsyncImage(url: URL(string: pokemon.isUnlocked ? pokemon.url : defaultUrl)) { image in
+                AsyncImage(url: URL(string: url)) { image in
                     image
                         .resizable()
                         .scaledToFit()
@@ -35,9 +42,10 @@ struct PokemonView: View {
                 .background(.thinMaterial)
                 .clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/)
                 .overlay {
-                    viewStyle == .surprise ? Image(.pokeball).resizable().frame(width: dimension, height: dimension) : nil
+                    viewStyle == .surprise ? Image(.pokeball).resizable().frame(width: dimension, height: dimension).opacity(selected ? 0 : 1) : nil
                 }
             }
+//            .disabled(viewStyle == .pokedex)
             Text("\(pokemon.isUnlocked ? pokemon.name.capitalized : "?????")")
                 .font(.system(size: 16, weight: .regular, design: .monospaced))
         }
@@ -53,5 +61,5 @@ extension PokemonView {
 
 #Preview {
     @State var pokemon: Pokemon = Pokemon(name: "bulbasaur", url: "https://pokeapi.co/api/v2/pokemon/1/")
-    return PokemonView(viewStyle: .surprise, pokemon: $pokemon)
+    return PokemonView(viewStyle: .surprise, pokemon: $pokemon).environmentObject(PokemonManager())
 }
