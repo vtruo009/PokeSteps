4aa6936d6a0e041e559054466c214ab82a4ae341
Van Truong <van.t414@gmail.com>
2024-02-24 17:34:58 -0800

    add class to store app colors

---
+++ PokeSteps/ViewModels/AppColor.swift
@@ -0,0 +1,16 @@
+//
+//  AppColor.swift
+//  PokeSteps
+//
+//  Created by Van Truong on 2/24/24.
+//
+
+import Foundation
+import SwiftUI
+
+class AppColor {
+    static var darkBlue: Color = Color(red: 60/255, green: 90/255, blue: 166/255)
+    static var lightBlue: Color = Color(red: 42/255, green: 117/255, blue: 187/255)
+    static var darkYellow: Color = Color(red: 199/255, green: 160/255, blue: 8/255)
+    static var lightYellow: Color = Color(red: 1, green: 203/255, blue: 5/255)
+}
